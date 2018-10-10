2ac66947601cec83f8b06a0e6a74098b3bb9cac7
Tom Chege <emdeechege@gmail.com>
2018-10-10 07:29:44 +0300

    added templates to register new clients

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http  import HttpResponse,Http404,HttpResponseRedirect
+from .models import *
 # Create your views here.
+@login_required(login_url='/accounts/login/')
+def home(request):
+
+    message ='See me?'
+    return render(request,"home.html",{"message":message})
