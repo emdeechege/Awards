e99d4c2078107d71dd55833238aef9781c70925f
Tom Chege <emdeechege@gmail.com>
2018-10-10 11:46:03 +0300

    initial homepage styling for navigation

--- projects/views.py
+++ projects/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http  import HttpResponse,Http404,HttpResponseRedirect
 from .models import *
 # Create your views here.
-@login_required(login_url='/accounts/login/')
+
 def home(request):
 
     message ='See me?'
