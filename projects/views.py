d25b26d33e8631cb9f7cf8fb506490a845528315
Tom Chege <emdeechege@gmail.com>
2018-10-11 11:02:38 +0300

    mock html for testing added

--- projects/views.py
+++ projects/views.py
@@ -22,3 +22,60 @@ def home(request):
         form = ReviewForm()
 
     return render(request,"home.html",{"projects":projects, "reviews":reviews,"form": form,"profile":profile})
+
+@login_required
+def profile(request,profile_id):
+
+    profile = Profile.objects.get(pk = profile_id)
+    projects = Project.objects.filter(profile_id=profile).all()
+
+    return render(request,"profile.html",{"profile":profile,"projects":projects})
+
+@login_required(login_url='/accounts/login/')
+def add_profile(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.user = current_user
+            profile.save()
+        return redirect('home')
+
+    else:
+        form = NewProfileForm()
+    return render(request, 'new_profile.html', {"form": form})
+
+@login_required(login_url='/accounts/login/')
+def update_project(request):
+    current_user = request.user
+    profiles = Profile.get_profile()
+    for profile in profiles:
+        if profile.user.id == current_user.id:
+            if request.method == 'POST':
+                form = UploadForm(request.POST,request.FILES)
+                if form.is_valid():
+                    upload = form.save(commit=False)
+                    upload.posted_by = current_user
+                    upload.profile = profile
+                    upload.save()
+                    return redirect('home')
+            else:
+                form = UploadForm()
+            return render(request,'upload.html',{"user":current_user,"form":form})
+
+@login_required(login_url='/accounts/login/')
+def add_review(request,pk):
+    image = get_object_or_404(Image, pk=pk)
+    current_user = request.user
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.image = image
+            comment.poster = current_user
+            comment.save()
+            return redirect('home')
+    else:
+        form = ReviewForm()
+        return render(request,'creview.html',{"user":current_user,"review_form":form})
